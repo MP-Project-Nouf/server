cabad20da8fd4f3a51940491bc89c988c68801db
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-20 11:19:03 +0300

    create role controller

---
+++ routers/controllers/role.js
@@ -0,0 +1,18 @@
+const roleModel = require("./../../db/models/role");
+
+const addRole = (req, res) => {
+    const { role } = req.body;
+    const newRole = new roleModel({
+      role,
+    });
+    newRole
+      .save()
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch((err) => {
+          res.status(400).json(err);
+      });
+  };
+
+module.exports =  addRole ;
