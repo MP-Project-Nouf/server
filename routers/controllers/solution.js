91bd6d33dbc287960463933a3ff69059fa05cf95
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-27 16:44:38 +0300

    edit

--- routers/controllers/solution.js
+++ routers/controllers/solution.js
@@ -1,8 +1,9 @@
 const solutionModel = require("./../../db/models/solution");
+const userModel = require("./../../db/models/user");
 
 //start addsolution function
 const addsolution = (req, res) => {
-  const { image, username, solve, challenge,point,title } = req.body;
+  const { image, username, solve, challenge, point, title } = req.body;
   const userId = req.token.id;
   const newSolutionModel = new solutionModel({
     image,
@@ -16,7 +17,24 @@ const addsolution = (req, res) => {
   newSolutionModel
     .save()
     .then((result) => {
-      res.status(200).json(result);
+      userModel
+        .findOne({ _id: userId })
+        .then(async (resul) => {
+          // console.log("resul.level",resul.level)
+          const leve = resul.level + 1;
+          const newPoint =resul.point+point;
+          console.log("leve",leve)
+          const updateRes = await userModel.findByIdAndUpdate(
+            { _id: userId },
+            { level: leve ,point:newPoint},
+            { new: true }
+          );
+          res.status(200).json(updateRes);
+        })
+        .catch((error) => {
+          console.log("error level", error);
+          res.status(400).json(err);
+        });
     })
     .catch((err) => {
       res.status(400).json(err);
@@ -26,22 +44,22 @@ const addsolution = (req, res) => {
 
 //start getSoulByuser function
 const getSoulByuser = (req, res) => {
-    const { user } = req.params;
-    solutionModel
-      .find({ user })
-      .then((result) => {
-        if (result) {
-          res.status(200).json(result);
-        } else {
-          res.status(404).json("not found any solution");
-        }
-      })
-      .catch((err) => {
-        console.log("err", err);
-        res.status(400).json(err);
-      });
-  };
-  //end getSoulByuser function
+  const { user } = req.params;
+  solutionModel
+    .find({ user })
+    .then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json("not found any solution");
+      }
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.status(400).json(err);
+    });
+};
+//end getSoulByuser function
 
 //start getSoulByChall function
 const getSoulByChall = (req, res) => {
@@ -62,4 +80,4 @@ const getSoulByChall = (req, res) => {
 };
 //end getSoulByChall function
 
-module.exports = { addsolution, getSoulByChall,getSoulByuser };
+module.exports = { addsolution, getSoulByChall, getSoulByuser };
